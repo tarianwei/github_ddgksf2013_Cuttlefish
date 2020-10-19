de6d5aab0ffff4bbe91bcc07a8319c6f87f19c87
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2020-10-19 19:33:32 +0800

    Create xxys.js

---
+++ Crack/xxys.js
@@ -0,0 +1,28 @@
+/*
+更新：2020-10-15
+小小影视 unlock Vip&免广告
+感谢@GB-png，提供新方法
+QX:
+https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) url script-response-body xxys.js
+Surge:
+http-response https:\/\/.*\..*\.com\/(ssp-svr\/ssp\/list3|ucp/index) requires-body=1,max-size=0,script-path= xxys.js
+MITM = *.xxjjappss.com,*.huaerdadi.com
+*/
+
+const path1 = "/ucp/index";
+const ad = 'ssp-svr/ssp/list3';
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+	obj.data.uinfo["down_daily_remainders"] = "666";
+	obj.data.uinfo["play_daily_remainders"] = "666";
+	obj.data.uinfo["curr_group"] = "5";
+	obj.data.user["isvip"] = "1";
+	obj.data.user["goldcoin"] = "666";
+}
+
+if ($request.url.indexOf(ad) != -1) {
+delete obj.data.pmap
+}
+
+$done({body: JSON.stringify(obj)});
