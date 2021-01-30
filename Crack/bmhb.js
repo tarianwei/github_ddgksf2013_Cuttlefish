1c5340174534905c0def035f684a45d0131171ce
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2021-01-30 19:09:20 +0800

    Update bmhb.js

--- Crack/bmhb.js
+++ Crack/bmhb.js
@@ -6,5 +6,4 @@ let body= $response.body;
 var obj = JSON.parse(body); 
 obj.data.business.is_lifelong_vip = true;
 obj.data.business.wool = 20210130;
-obj.data.profile.nickname = “墨鱼手记”;
 $done({body: JSON.stringify(obj)});
