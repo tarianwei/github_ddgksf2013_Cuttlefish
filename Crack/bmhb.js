bc6e313158aa3895de429734526d746b35dc1c58
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2021-01-30 19:01:44 +0800

    Update bmhb.js

--- Crack/bmhb.js
+++ Crack/bmhb.js
@@ -4,7 +4,7 @@ Unlocks by photonmang 公众号：墨鱼手记
 
 let body= $response.body; 
 var obj = JSON.parse(body); 
-obj.data.is_lifelong_vip = true;
-obj.data.wool = 20210130;
-obj.profile.nickname = “墨鱼手记”;
+obj.data.business.is_lifelong_vip = true;
+obj.data.business.wool = 20210130;
+obj.data.profile.nickname = “墨鱼手记”;
 $done({body: JSON.stringify(obj)});
