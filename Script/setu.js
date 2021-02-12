1e647a895c2fd49a0c4a4dff71a30d67759e818c
Cuttlefish <37397537+ddgksf2013@users.noreply.github.com>
2021-02-12 21:58:37 +0800

    Update setu.js

--- Script/setu.js
+++ Script/setu.js
@@ -16,18 +16,21 @@ let userR18      = 2 //18禁为1 非为0 2是混合
 let userKeyword  = ''//搜索关键字
 let userNum      = 1 //一次返回的结果数量，范围为1到10
 var request = {
-    url:encodeURI("https://api.lolicon.app/setu?apikey=" + userApikey + "&r18=" + userR18 + "&keyword=" + userKeyword),
+    url:encodeURI("https://api.lolicon.app/setu?apikey=" + userApikey + "&r18=" + userR18 + "&keyword=" + userKeyword + "&num=" + userNum),
     header:{  
      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Mobile/15E148 Safari/604.1"
     },
 }
 $task.fetch(request).then(response => {
 	let obj = JSON.parse(response.body);
+	console.log(response.body);
 	if(obj.code == 0)
 	{
-		let pictureURL = encodeURI(obj.data[0].url);
-		console.log(pictureURL);
-		$notify("每日色图", "", "元气满满", {"open-url":pictureURL,"media-url":pictureURL}); // Success
+		for(i = 0;i<obj.data.length;i++)
+		{
+			let pictureURL = encodeURI(obj.data[i].url);
+			$notify("每日色图", "", "cuttlefish", {"open-url":pictureURL,"media-url":pictureURL}); // Success
+		}
 	}
 	else
 	{
